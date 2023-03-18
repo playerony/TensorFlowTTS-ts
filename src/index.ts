c63e7793f1b58d0a81e51f13d5570d56489418b3
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-18 16:57:13 +0100

    build: condifure rollup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('test');
+import * as tf from '@tensorflow/tfjs';
+
+const loadGraphModel = async (name: string) =>
+  tf.loadGraphModel(
+    `https://raw.githubusercontent.com/playerony/TensorFlowTTS-ts/main/models/${name}.model.json`,
+  );
+
+const loadModels = async () => {
+  const vocoder = await loadGraphModel('vocoder');
+  const text2mel = await loadGraphModel('text2mel');
+
+  console.log(text2mel, vocoder);
+};
+
+loadModels();
