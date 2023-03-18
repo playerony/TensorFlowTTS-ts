e55ecca5361a89f9529055c47998544a1d082ff8
playerony <pawel.wojtasinski.1995@gmail.com>
2023-03-18 17:09:14 +0100

    feat: upload tiny models

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as tf from '@tensorflow/tfjs';
 
 const loadGraphModel = async (name: string) =>
   tf.loadGraphModel(
-    `https://raw.githubusercontent.com/playerony/TensorFlowTTS-ts/main/models/${name}.model.json`,
+    `https://raw.githubusercontent.com/playerony/TensorFlowTTS-ts/main/models/${name}/model.json`,
   );
 
 const loadModels = async () => {
