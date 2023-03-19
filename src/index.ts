2a727241d5e635600d9eda086572e904122df124
Paweł Wojtasiński <pawel.wojtasinski.1995@gmail.com>
2023-03-19 18:10:51 +0100

    feat: build simple UI (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,81 @@
+import * as tf from '@tensorflow/tfjs';
+
 import { playAudio } from './core/playAudio';
 import { cleanUpStringInput } from './core/cleanUpStringInput';
 import { getGraphModelByName } from './core/getGraphModelByName';
 import { convertStringToSequence } from './core/convertStringToSequence';
 import { generateVocoderOutputInstance } from './core/generateVocoderOutput';
 
-const loadModels = async () => {
-  const vocoderModel = await getGraphModelByName('vocoder');
-  const text2melModel = await getGraphModelByName('text2mel');
-  const generateVocoderOutput = generateVocoderOutputInstance(vocoderModel, text2melModel);
+const getButtonElement = () => document.getElementById('speak-button') as HTMLButtonElement;
 
-  const parsedInput = cleanUpStringInput('Test input string etc.');
-  const inputSequence = convertStringToSequence(parsedInput);
-  const vocoderOutput = await generateVocoderOutput(inputSequence);
+const getTextareaElement = () => document.getElementById('text-to-read-out') as HTMLInputElement;
 
-  playAudio(vocoderOutput);
-};
-
-const assignOnClickEvent = () => {
-  const buttonElement = document.querySelector('button');
+const toggleButtonLoadingState = (buttonElement: HTMLButtonElement, isLoading: boolean) => {
   if (!buttonElement) {
     return;
   }
 
-  buttonElement.addEventListener('click', loadModels);
+  buttonElement.classList[isLoading ? 'add' : 'remove']('is-loading');
 };
 
-window.onload = assignOnClickEvent;
+const initializeModels = async () => {
+  const vocoderModel = await getGraphModelByName('vocoder');
+  const text2melModel = await getGraphModelByName('text2mel');
+
+  return { vocoderModel, text2melModel };
+};
+
+const speak = async (input: string, vocoderModel: tf.GraphModel, text2melModel: tf.GraphModel) => {
+  const parsedInput = cleanUpStringInput(input);
+  const inputSequence = convertStringToSequence(parsedInput);
+  const vocoderOutput = await generateVocoderOutputInstance(
+    vocoderModel,
+    text2melModel,
+  )(inputSequence);
+
+  await playAudio(vocoderOutput);
+};
+
+const handleButtonClick = async (
+  event: MouseEvent,
+  vocoderModel: tf.GraphModel,
+  text2melModel: tf.GraphModel,
+) => {
+  const inputElement = getTextareaElement();
+  if (!inputElement) {
+    return;
+  }
+
+  const input = inputElement.value;
+  if (!input) {
+    return;
+  }
+
+  const buttonElement = event.target as HTMLButtonElement;
+  toggleButtonLoadingState(buttonElement, true);
+  await speak(input, vocoderModel, text2melModel);
+  toggleButtonLoadingState(buttonElement, false);
+};
+
+const assignOnClickEventToButton = (vocoderModel: tf.GraphModel, text2melModel: tf.GraphModel) => {
+  const buttonElement = getButtonElement();
+  if (!buttonElement) {
+    return;
+  }
+
+  buttonElement.addEventListener('click', (event) =>
+    handleButtonClick(event, vocoderModel, text2melModel),
+  );
+};
+
+const onLoad = async () => {
+  const buttonElement = getButtonElement();
+  toggleButtonLoadingState(buttonElement, true);
+
+  const { vocoderModel, text2melModel } = await initializeModels();
+
+  toggleButtonLoadingState(buttonElement, false);
+  assignOnClickEventToButton(vocoderModel, text2melModel);
+};
+
+window.onload = onLoad;
